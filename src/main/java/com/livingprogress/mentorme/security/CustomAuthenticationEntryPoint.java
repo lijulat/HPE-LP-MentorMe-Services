a6b54ec59014e9835df21b0317b3d84c23a99931
kinfkong <kinfkong@126.com>
2017-01-24 12:47:13 +0800

    modify smtp config

--- src/main/java/com/livingprogress/mentorme/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/livingprogress/mentorme/security/CustomAuthenticationEntryPoint.java
@@ -11,14 +11,12 @@ import java.io.IOException;
 public class CustomAuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
 
     public CustomAuthenticationEntryPoint() {
-        System.out.println("Here...");
         this.setRealmName("mentorme");
     }
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
             throws IOException, ServletException {
-        System.out.println("Here 2...");
         response.setHeader("WWW-Authenticate", "FormBased");
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
     }
