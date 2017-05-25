f6d390d1028efde6023c043e3a2362f2943a0484
Gaurav Seta <Gauravseta@gmail.com>
2017-05-25 13:21:50 +0530

    Update MenteeController.java

--- src/main/java/com/livingprogress/mentorme/controllers/MenteeController.java
+++ src/main/java/com/livingprogress/mentorme/controllers/MenteeController.java
@@ -185,8 +185,7 @@ public class MenteeController extends BaseEmailController {
 	   String localeValue = localeObj.getValue();
 	   Locale localeObjTemp = mentorService.findLocaleByValue(localeValue); // fetch the Locale Instance
 	   entity.setLocale(localeObjTemp);                              // Set in the entity 
-	}
-	else {
+	} else {
 	   Locale localeObjTemp = mentorService.findLocaleByValue("en");        // get locale Instance having "en" by default
 	   entity.setLocale(localeObjTemp);                              // Set in the entity by default english language
 	}
